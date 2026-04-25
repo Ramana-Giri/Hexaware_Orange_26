71c1d13efa0ce7fb9922c7adb394ad8c6a8a344f
RG <loamramg@gmail.com>
2026-04-26 00:54:36 +0530

    Complete workflow of amazon web site - from login to payment - learn switchTo - learn wait - learn scroll using JavascriptExecutor

--- src/main/java/day2/Day2_Amazon.java
+++ src/main/java/day2/Day2_Amazon.java
@@ -12,6 +12,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.locators.RelativeLocator;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -37,27 +38,37 @@ public class Day2_Amazon {
 		selectProduct();
 		switchToProductHandle(parent_handle);
 		addProductToCart();
+		addPaymentMethod();
 //		driver.quit();
+	}
+
+	private static void addPaymentMethod() {
+		driver.findElement(By.xpath("//input[@value='SelectableAddCreditCard']")).click();
+		
+		wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.partialLinkText("Add a new credit or debit card")));
+		driver.findElement(By.partialLinkText("Add a new credit or debit card")).click();
+		
+		wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.cssSelector("iframe[name='ApxSecureIframe']")));
+		driver.findElement(By.name("addCreditCardNumber")).sendKeys("12131234567");
+		driver.findElement(By.name("ppw-accountHolderName")).sendKeys("abcd");
+		Select monthSelect = new Select(driver.findElement(By.name("ppw-expirationDate_month")));
+		monthSelect.selectByVisibleText("05");
+		Select yearSelect = new Select(driver.findElement(By.name("ppw-expirationDate_year")));
+		yearSelect.selectByVisibleText("2028");
 	}
 
 	private static void addProductToCart() throws InterruptedException {
 //		driver.get("https://www.amazon.in/vivo-X300-Pro-Additional-Exchange/dp/B0G26FN2MK/ref=sr_1_6?dib=eyJ2IjoiMSJ9.lFGsBaNdfbLuYPa9gAhRSJzz2sFp3xvlQpd453pMzHXgI7yH0KMFS3Vr47BkCuHtM_hcgTxXADdKS5wJQ9P2iKmJo-Yl17OVfD5f9hbukaB9uucr2_cPkMLvaoMkGfsJFtP33FPzhqKj7zcWUiY8UASEpdPenvEKv-5pQVttAbf1HxhTJAxYGof_Yp2tHPcJAQyD1RZyy5631ztBq2e95SgJE-xvdHaLtihZI56bjMM.y8tBa4Sjcg6B88J-9_OAJEcfZl0sbtGgq9GZYAlUKJk&dib_tag=se&keywords=vivo+x+series+phones&qid=1777117386&sr=8-6");
 		WebElement cartButton = driver.findElement(By.xpath("//input[@title='Buy Now']"));
 		System.out.println("buy button found");
-//		JavascriptExecutor js = (JavascriptExecutor) driver;
-//		js.executeScript("window.scrollBy(0, 1000)");
 		driver.findElement(RelativeLocator.with(By.tagName("input")).above(cartButton)).click();
 		System.out.println("Clicked add to cart button");
-		
-		WebElement cartBtn = wait.until(ExpectedConditions.elementToBeClickable(
-			    By.cssSelector("#hctp-attach-side-sheet input.a-button-input")
-			));
+		WebElement cartBtn = wait.until(ExpectedConditions
+				.elementToBeClickable(By.cssSelector("#hctp-attach-side-sheet input.a-button-input")));
 
 		cartBtn.click();
-		
-//		wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//input[text()=' Added to cart ']")));
-//		WebElement cartSpan = driver.findElement(By.xpath("//input[text()=' Cart ']"));
-//		driver.findElement(RelativeLocator.with(By.tagName("input")).above(cartSpan)).click();
+
+		driver.findElement(By.name("proceedToRetailCheckout")).click();
 	}
 
 	private static void switchToProductHandle(String parent_handle) {
@@ -66,7 +77,7 @@ public class Day2_Amazon {
 		System.out.println(handles);
 		for (String handle1 : handles) {
 			if (!handle1.equals(parent_handle)) {
-				
+
 				driver.switchTo().window(handle1);
 				System.out.println("Swithced to new window : " + handle1);
 				break;
@@ -85,7 +96,7 @@ public class Day2_Amazon {
 	private static void searchProduct() {
 		WebElement searchBar = driver.findElement(By.id("twotabsearchtextbox"));
 		searchBar.sendKeys("vivo x series phones");
-		
+
 		driver.findElement(RelativeLocator.with(By.tagName("input")).near(searchBar)).click();
 	}
 
@@ -96,7 +107,7 @@ public class Day2_Amazon {
 	private static void login() {
 		Dotenv dotenv = Dotenv.load();
 		String password = dotenv.get("AMAZON_PASSWORD");
-		
+
 		driver.findElement(By.cssSelector("a[data-nav-ref=\"nav_ya_signin\"]")).click();
 
 		WebElement loginField = driver.findElement(By.id("ap_email_login"));
@@ -104,7 +115,7 @@ public class Day2_Amazon {
 		loginField.sendKeys("loamramg@gmail.com");
 
 		driver.findElement(RelativeLocator.with(By.tagName("input")).below(loginField)).click();
-		
+
 		WebElement passwordField = driver.findElement(By.name("password"));
 		passwordField.sendKeys(password);
 
